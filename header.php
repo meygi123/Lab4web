f4a699d1d4f623e4edde2e9de148148621fc743b
LAPTOP-D1T03I99\Meygi <meygilamonda>
2023-04-02 21:23:29 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Contoh Modularisasi</title>
+<link rel="stylesheet" type="text/css" href="style.css"media="screen">
+</head>
+<body>
+<div class="container">
+<header>
+<h1>Modularisasi Menggunakan Require</h1>
+</header>
+<nav>
+		<ul>
+			<li><a href="home.php">Home</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="kontak.php">Kontak</a></li>
+		</ul>
+	</nav>
